46de9e625189fb8601ba9305714ffe2c905156ad
pavel.belyakov <p.belyakov@south.rt.ru>
2025-07-26 16:47:56 +0400

    added allure steps TextBoxJenkinsTests

--- src/test/java/tests/lesson_18/TextBoxJenkinsTests.java
+++ src/test/java/tests/lesson_18/TextBoxJenkinsTests.java
@@ -1,13 +1,19 @@
 package tests.lesson_18;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import utils.GenerateFakeData;
 
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
 @Tag("demoqa")
+@DisplayName("Тесты заполнения формы регистрации")
 public class TextBoxJenkinsTests {
 
   RegistrationPage registrationPage = new RegistrationPage();
@@ -35,62 +41,87 @@ public class TextBoxJenkinsTests {
     Configuration.browserSize = "1920x1080";
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    SelenideLogger.addListener("allure", new AllureSelenide());
   }
 
   @Test
+  @DisplayName("Проверка заполнения обязательных полей")
   void minFillFormTest() {
-    registrationPage.openPage()
-            .deleteBanner()
-            .setFirstName(firstName)
-            .setLastName(lastName)
-            .setGender(gender)
-            .setUserNumber(phone)
-            .clickSubmit()
+    step("Открываем форму регистрации", () -> {
+      registrationPage.openPage()
+              .deleteBanner();
+    });
 
-            .checkPositiveResult("Student Name", firstName + " " + lastName)
-            .checkPositiveResult("Gender", gender)
-            .checkPositiveResult("Mobile", phone);
+    step("Заполняем обязательные поля", () -> {
+      registrationPage.setFirstName(firstName)
+              .setLastName(lastName)
+              .setGender(gender)
+              .setUserNumber(phone)
+              .clickSubmit();
+    });
+
+    step("Открылась таблица с обязательными данными", () -> {
+      registrationPage.checkPositiveResult("Student Name", firstName + " " + lastName)
+              .checkPositiveResult("Gender", gender)
+              .checkPositiveResult("Mobile", phone);
+    });
   }
 
   @Test
+  @DisplayName("Проверка заполнения всех полей")
   void fullFillFormTest() {
-    registrationPage.openPage()
-            .deleteBanner()
-            .setFirstName(firstName)
-            .setLastName(lastName)
-            .setUserEmail(email)
-            .setGender(gender)
-            .setUserNumber(phone)
-            .setBirthDate(day, month, year)
-            .setSubject(subject)
-            .setHobby(hobby)
-            .uploadPicture(picture)
-            .setAddress(address)
-            .setState(state)
-            .setCity(city)
-            .clickSubmit()
+    step("Открываем форму регистрации", () -> {
+      registrationPage.openPage()
+              .deleteBanner();
+    });
 
-            .checkPositiveResult("Student Name", firstName + " " + lastName)
-            .checkPositiveResult("Student Email", email)
-            .checkPositiveResult("Gender", gender)
-            .checkPositiveResult("Mobile", phone)
-            .checkPositiveResult("Date of Birth", day + " " + month + "," + year)
-            .checkPositiveResult("Subjects", subject)
-            .checkPositiveResult("Hobbies", hobby)
-            .checkPositiveResult("Picture", picture)
-            .checkPositiveResult("Address", address)
-            .checkPositiveResult("State and City", state + " " + city);
+    step("Заполняем все поля", () -> {
+      registrationPage.setFirstName(firstName)
+              .setLastName(lastName)
+              .setUserEmail(email)
+              .setGender(gender)
+              .setUserNumber(phone)
+              .setBirthDate(day, month, year)
+              .setSubject(subject)
+              .setHobby(hobby)
+              .uploadPicture(picture)
+              .setAddress(address)
+              .setState(state)
+              .setCity(city)
+              .clickSubmit();
+    });
+
+    step("Открылась таблица со всеми данными", () -> {
+      registrationPage.checkPositiveResult("Student Name", firstName + " " + lastName)
+              .checkPositiveResult("Student Email", email)
+              .checkPositiveResult("Gender", gender)
+              .checkPositiveResult("Mobile", phone)
+              .checkPositiveResult("Date of Birth", day + " " + month + "," + year)
+              .checkPositiveResult("Subjects", subject)
+              .checkPositiveResult("Hobbies", hobby)
+              .checkPositiveResult("Picture", picture)
+              .checkPositiveResult("Address", address)
+              .checkPositiveResult("State and City", state + " " + city);
+    });
   }
 
   @Test
+  @DisplayName("Проверка без заполнения обязательного поля - фамилии")
   void withoutLastNameFillFormTest() {
-    registrationPage.openPage()
-            .deleteBanner()
-            .setFirstName(firstName)
-            .setGender(gender)
-            .setUserNumber(phone)
-            .clickSubmit()
+    step("Открываем форму регистрации", () -> {
+      registrationPage.openPage()
+              .deleteBanner();
+    });
 
-            .checkNegativeResult();
+    step("Заполняем обязательные поля без указания фамилии", () -> {
+      registrationPage.setFirstName(firstName)
+              .setGender(gender)
+              .setUserNumber(phone)
+              .clickSubmit();
+    });
+
+    step("Форма не сохранилась", () -> {
+      registrationPage.checkNegativeResult();
+    });
   }
 }
